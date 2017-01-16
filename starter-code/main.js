9cb899511f80ecfb2b3c35418774cae37caccb94
Boone Reynolds <boonereynolds@Boones-MacBook-Pro.local>
2017-01-16 16:15:15 -0600

    add if statements and alerts

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,21 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardTwo === cardFour){
+	alert ("You found a match!");
+} else {
+	alert ("Sorry, try again.")
+}
+// queen comparison
+if (cardOne === cardTwo){
+	alert ("You found a match!");
+} else {
+	alert ("Sorry, try again.")
+}
+// king comparison 
+if (cardThree === cardFour){
+	alert ("You found a match!");
+} else {
+	alert ("Sorry, try again.")
+}
