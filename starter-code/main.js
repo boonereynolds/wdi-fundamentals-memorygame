8f55a3189e9408f52b8fd9132a07e6a466649f24
Boone Reynolds <boonereynolds@Boones-MacBook-Pro.local>
2017-01-17 21:50:46 -0600

    generate card function added

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,20 +3,32 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardTwo === cardFour){
-	alert ("You found a match!");
-} else {
-	alert ("Sorry, try again.")
+// if (cardTwo === cardFour){
+// 	alert ("You found a match!");
+// } else {
+// 	alert ("Sorry, try again.")
+// }
+// // queen comparison
+// if (cardOne === cardTwo){
+// 	alert ("You found a match!");
+// } else {
+// 	alert ("Sorry, try again.")
+// }
+// // king comparison 
+// if (cardThree === cardFour){
+// 	alert ("You found a match!");
+// } else {
+// 	alert ("Sorry, try again.")
+// }
+
+function createCards(){
+	var gameboard = document.getElementById("game-board");
+
+	for(var i = 0; i < 4; i ++){
+		var card = document.createElement('div');
+		card.className = 'card';
+		gameboard.appendChild(card);
+	}
 }
-// queen comparison
-if (cardOne === cardTwo){
-	alert ("You found a match!");
-} else {
-	alert ("Sorry, try again.")
-}
-// king comparison 
-if (cardThree === cardFour){
-	alert ("You found a match!");
-} else {
-	alert ("Sorry, try again.")
-}
+
+createCards();
